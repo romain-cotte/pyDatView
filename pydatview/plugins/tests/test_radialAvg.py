dcab4b40df0a99516093fc161cff0d848396bce1
Romain Cotte <romain.cotte@gmail.com>
2023-07-20 12:52:19 +0200

    Pipeline: add skipTest when DISPLAY not set for raddialAvg

--- pydatview/plugins/tests/test_radialAvg.py
+++ pydatview/plugins/tests/test_radialAvg.py
@@ -1,5 +1,7 @@
 import unittest
 import numpy as np
+import os
+
 from pydatview.plugins.base_plugin import demoGUIPlugin, HAS_WX
 from pydatview.plugins.data_radialavg import *
 from pydatview.plugins.data_radialavg import _DEFAULT_DICT
@@ -7,10 +9,10 @@ from pydatview.plugins.data_radialavg import _DEFAULT_DICT
 class TestRadialAvg(unittest.TestCase):
 
     def test_showGUI(self):
-        if HAS_WX:
-            demoGUIPlugin(RadialToolPanel, actionCreator=radialAvgAction, mainLoop=False, title='Radial Avg')
-        else:
-            print('[WARN] skipping test because wx is not available.')
+        if not HAS_WX or not os.environ.get("DISPLAY"):
+            self.skipTest("[WARN] skipping test because wx or DISPLAY is not available.")
+
+        demoGUIPlugin(RadialToolPanel, actionCreator=radialAvgAction, mainLoop=False, title='Radial Avg')
 
 if __name__ == '__main__':
     unittest.main()
